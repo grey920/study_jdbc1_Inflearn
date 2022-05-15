c9d9f204d1f9d706a1a1d2542d8b0ea9f95e7e11
grey <charminggw@gmail.com>
2022-05-16 07:48:02 +0900

    JDBC 개발 - 조회/수정/삭제

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -44,6 +45,105 @@ public class MemberRepositoryV0 {
             close( con, pstmt, null );
             }
 
+
+    }
+
+    public Member findById( String memberId ) throws SQLException {
+
+        String sql = "select * from Member where member_id = ?";
+
+        Connection con = null; // finally 구문에서 호출해야 하기 떄문에 밖으로 빼서 선언한다
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId );
+
+            // executeUpdate()는 변경할때 사용하고, select 쿼리는 executeQuery()를 사용한다
+            rs = pstmt.executeQuery();
+
+            // rs.next()를 한 번은 호출해줘야 그 다음부터 실제 응답을 출력한다
+            if ( rs.next() ){
+
+                Member member = new Member();
+                member.setMemberId( rs.getString("member_id") );
+                member.setMoney( rs.getInt("money") );
+                return member;
+            }
+            // 데이터가 없는 경우
+            else{
+                /* 에러 메시지를 작성할 떄 key값을 잘 넣어줘야 실제 운영에서 문제를 찾을 때 편리하다 */
+                throw new NoSuchElementException("member not found, memberId = " + memberId );
+            }
+        } catch (SQLException e) {
+            log.error( "db error", e );
+            throw e;
+        }
+        finally {
+            close( con, pstmt, rs );
+        }
+
+    }
+
+    public void update( String memberId, int money ) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+
+            con = getConnection();
+            pstmt = con.prepareStatement( sql );
+            // 파라미터 바인딩
+            pstmt.setInt(1, money );
+            pstmt.setString(2, memberId );
+
+            // 실행
+            int count = pstmt.executeUpdate(); // count: 영향받은 row 수
+            log.info( "result size={}", count );
+
+        }
+        // error log 남기고 밖으로 Exception 던지기
+        catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }
+        // [중요!] 오픈과 역순으로 닫아주기! 항상 닫는것이 보장되도록 finally에서 리소스를 정리하기
+        // TCP/IP 커넥션에 걸려서 외부 리소스를 쓰는 것이기 때문에 닫아주지 않으면 계~~속 유지될 수 있다.
+        finally {
+            close( con, pstmt, null );
+        }
+
+    }
+
+    public void delete( String memberId ) throws SQLException {
+
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+
+            con = getConnection();
+            pstmt = con.prepareStatement( sql );
+            pstmt.setString(1, memberId );
+
+            // 실행
+            int count = pstmt.executeUpdate(); // count: 영향받은 row 수
+            log.info( "result size={}", count );
+
+        }
+        catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }
+        finally {
+            close( con, pstmt, null );
+        }
 
     }
 
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,52 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV1", 10000);
+        /* save */
+        Member member = new Member( "memberV100", 10000 );
         repository.save( member );
+
+        /* findById */
+        Member findMember = repository.findById( member.getMemberId() );
+        log.info( "findMember={}", findMember );
+
+        // member == findMember false : member와 findMember는 다른 인스턴스이다. findMember는 ResultSet에서 담을때 생성한 인스턴스
+        log.info( "member == findMember {}", member == findMember );
+
+        // member.equals( findMember ) true
+        // 이유: Member객체 만들때 사용한 @Data 롬복은 EqualsAndHashCode를 가지고 있다. 따라서 Member객체의 필드값들이 같다면 equals는 true가 된다
+        log.info( "member.equals( findMember ) {}", member.equals( findMember ) );
+
+        // 찾은 멤버가 방금 등록한 멤버와 같다
+        assertThat( findMember ).isEqualTo( member );
+
+        /* update: money : 10000 -> 20000 */
+        repository.update( member.getMemberId(), 20000 );
+        Member updatedMember = repository.findById( member.getMemberId() );
+        assertThat( updatedMember.getMoney() ).isEqualTo( 20000 );
+
+        /* delete -> 테스트 중간에 오류가 발생하는 경우 삭제 로직이 수행되지 않기 때문에 아래같은 코드는 좋은 코드는 아님
+        * => 트랜잭션 활용!!!*/
+        repository.delete( member.getMemberId() );
+        // 방금 생성한 멤버가 NoSuchElement 익셉션을 던지면 정상적으로 삭제되었다고 판단한다.
+        Assertions.assertThatThrownBy( () -> repository.findById( member.getMemberId() ) )
+                .isInstanceOf( NoSuchElementException.class );
     }
+
 }
